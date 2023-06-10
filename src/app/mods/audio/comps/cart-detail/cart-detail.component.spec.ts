196ceed2bb53fe20d6cd82b1dc0ffebd5c603ed6
rinebob <rinebob111185@gmail.com>
2023-06-10 09:26:16 -0700

    [Testing] 6 - Implements tests for CartDetail and related components

--- src/app/mods/audio/comps/cart-detail/cart-detail.component.spec.ts
+++ src/app/mods/audio/comps/cart-detail/cart-detail.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AudioModule } from '../../audio.module';
 import { CartDetailComponent } from './cart-detail.component';
+import { CartDetailMode } from '../../common/au-interfaces';
 
 describe('CartDetailComponent', () => {
   let component: CartDetailComponent;
@@ -18,6 +19,38 @@ describe('CartDetailComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
+
+  // async function setup(
+  //     formValidity: string,
+  //   ) {
+  //   await TestBed.configureTestingModule({
+
+  //   }).compileComponents();
+  // }
+
+  // When embedded in CheckoutPage component
+  describe('summary mode', () => {
+
+    // has headline text
+
+    // shows empty cart message if no products are in cart
+
+    // has list of products in cart
+
+    // shows correct number of product cards in UI
+
+    // product card has title and cost
+
+
+
+  });
+
+  // As dialog opened when NavBar ShowCart button is clicked
+  describe('detail mode', () => {
+
+  });
+
+
 
   it('should create', () => {
     expect(component).toBeTruthy();
